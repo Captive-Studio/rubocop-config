5a6c71a11435c4befeb41f4b6ddb84c99c8c1167
Clément Prod'homme <clement.prod-homme@captive.fr>
2023-04-29 16:16:20 +0200

    ✨ Ajoute un cop pour empêcher d'utiliser la variable d'environnement APP_ENV
    Cette variable vient remplacer RAILS_ENV avec Sidekiq !
    
    Ce qui nous a provoqué par le passé des problèmes, qui étaient complexe pour retrouver l'origine

---
+++ lib/rubocop/cop/captive/no_app_env.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Captive
+      # This cop checks for usages of the `APP_ENV` environment variable.
+      # Usage of `APP_ENV` is prohibited as it conflicts with standard Rails/Rack
+      # environment variable `RAILS_ENV` and may lead to unexpected results.
+      class NoAppEnv < RuboCop::Cop::Cop
+        MSG = "Do not use `APP_ENV` environment variable as it conflicts with "\
+              "standard Rails/Rack environment variable `RAILS_ENV`."
+
+        def_node_matcher :env_variable?, <<~PATTERN
+          (send
+            (const _ :ENV) {:[] :fetch}
+            (str "APP_ENV")
+            ...
+          )
+        PATTERN
+
+        def on_send(node)
+          return unless env_variable?(node)
+          add_offense(node, message: MSG)
+        end
+      end
+    end
+  end
+end
